7fcc95140c3c4195c1bdd8cfb79142cfe5b84c95
Ryan Snell <rasnell01@gmail.com>
2021-11-03 14:45:24 -0600

    Initial Commit

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Ryan A Snell on 11/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
